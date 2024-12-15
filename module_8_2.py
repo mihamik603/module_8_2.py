72a885f003e83e0d3083df928dd024c676e8848b
mihamik603 <vmm1331@gmail.com>
2024-12-15 21:55:53 +0400

    Update  module_8_2.py

--- module_8_2.py
+++ module_8_2.py
@@ -26,7 +26,6 @@ def calculate_average(numbers):
         return None
 
 
-# Примеры вызова функции calculate_average
 print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
